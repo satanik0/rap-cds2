17a5ae29fb878bcfa1a8e4aac3b45f9bfc3d9fbf
Satanik Ganguly <stnkganguly9@gmail.com>
2022-12-02 20:20:45 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("manageemp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
